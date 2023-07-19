f5681deec4f7a6183d2ebcc8043d60bc1831682e
Thomas Crespo <thomascrespo@bellsouth.net>
2023-07-18 20:47:02 -0400

    adding first draft of encode_decode.py to repo

---
+++ encode_decode.py
@@ -0,0 +1,44 @@
+'''
+Title: Lab 06: Github Encoder and Decoder
+Author: Thomas Crespo
+Date: 07/18/2023
+Code Version: Python 3.11
+'''
+
+#Simple Encode Function
+def encode(password):
+    resul = ''
+    temp = 0
+    for num in password:
+        temp = int(num) + 3
+        if temp >= 10:
+            temp = temp - 10
+        resul = resul + str(temp)
+    return resul
+
+#The Start of the Looping Encode and Decode Menu
+if __name__ == '__main__':
+    while True:
+        print(
+            'Menu\n'
+            '-------------\n'
+            '1. Encode\n'
+            '2. Decode\n'
+            '3. Quit\n'
+            ''
+        )
+        option = int(input('Please enter an option: '))
+        if option == 1:
+            passwor = input('Please enter your password to encode: ')
+            enpasswor = encode(passwor)
+            print('Your password has been encoded and stored!')
+            print()
+        elif option == 2:
+            print('The encoded password is ' + enpasswor +', and the original password is ' + passwor + '.')
+            print()
+        elif option == 3:
+             break
+        else:
+            print('Invalid Input')
+            continue
+        
